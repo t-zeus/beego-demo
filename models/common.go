d2f27209ec667abc9b2ed744506ab004458fd87f
netsan <netsan@126.com>
2019-01-23 22:32:17 +0800

    code info

--- models/common.go
+++ models/common.go
@@ -1,1 +1,15 @@
 package models
+
+// 定义 json 返回结构体
+type CodeInfo struct {
+	Code int    `json:"code"` // 对应 json 的 code
+	Msg  string `json:"msg"`  // 对应 json 的 msg
+}
+
+func NewErrorInfo(info string) *CodeInfo {
+	return &CodeInfo{-1, info}
+}
+
+func NewNormalInfo(info string) *CodeInfo {
+	return &CodeInfo{0, info}
+}
