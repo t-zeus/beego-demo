1df90449dedd009d355b1a3c42f6daa180e53767
netsan <netsan@126.com>
2019-01-24 09:27:24 +0800

    valid field tag

--- models/user_io.go
+++ models/user_io.go
@@ -2,7 +2,7 @@ package models
 
 // 注册表单结构
 type RegisterForm struct {
-	Phone    string `form:"phone"`
-	Name     string `form:"name"`
-	Password string `form:"password"`
+	Phone    string `form:"phone" valid:"Required"`
+	Name     string `form:"name" valid:"Required"`
+	Password string `form:"password" valid:"Required"`
 }
