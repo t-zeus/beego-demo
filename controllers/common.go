92d199b6ae1f8bdfb4d6486bfee5df395680a6ec
netsan <netsan@126.com>
2019-01-24 09:40:15 +0800

    verify form method

--- controllers/common.go
+++ controllers/common.go
@@ -1,5 +1,34 @@
 package controllers
 
+import (
+	"beego"
+	"errors"
+	"github.com/astaxie/beego/validation"
+)
+
 const (
 	ErrInputData = "数据输出错误"
 )
+
+// 父级
+type BaseController struct {
+	beego.Controller
+}
+
+// 在父级上封装表单验证函数
+func (c *BaseController) VerifyForm(v interface{}) error {
+	valid := validation.Validation{}
+	ok, err := valid.Valid(&v)
+	if err != nil {
+		return err
+	}
+	// 参数验证不通过
+	if !ok {
+		msg := ""
+		for _, err := range valid.Errors {
+			msg += err.Key + ":" + err.Message + ";"
+		}
+		return errors.New(msg)
+	}
+	return nil
+}
